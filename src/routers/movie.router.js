48828cddab0006fc3758abf03520e3c3f9b83bbd
ahmedhafez1089 <ahmedsaiedhafez16@yahoo.com>
2019-12-11 16:54:00 -0800

    Save photo on file system

--- src/routers/movie.router.js
+++ src/routers/movie.router.js
@@ -3,8 +3,6 @@ const router = new express.Router()
 
 const MovieController = require('../controllers/movie.controller')
 
-const apiKey = '38b114a997ec654bd7933e888cbf4921'
-
 router.get('/movie/:id', MovieController.getMovieDetails)
 
 router.get('/movie/:id/credits', MovieController.getMovieCredits)
